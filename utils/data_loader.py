6ebc07fc887fd72f34f906598c3c5a4ac3e5d0cb
a01736844-ivanna <a01736844@tec.mx>
2025-11-25 21:56:37 -0600

    data

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 @st.cache_resource
 def load_all_data():
-    dfb = pd.read_csv("/Users/hijos/Desktop/Analitica Prof/Prof. Alfredo/FINAL 2/data/Barcelona_Limpios.csv")
-    dfc = pd.read_csv("/Users/hijos/Desktop/Analitica Prof/Prof. Alfredo/FINAL 2/data/Cambridge_Limpios.csv")
-    dfbo = pd.read_csv("/Users/hijos/Desktop/Analitica Prof/Prof. Alfredo/FINAL 2/data/Boston_Limpios.csv")
-    dfh = pd.read_csv("/Users/hijos/Desktop/Analitica Prof/Prof. Alfredo/FINAL 2/data/Hawai_Limpios.csv")
-    dfbu = pd.read_csv("/Users/hijos/Desktop/Analitica Prof/Prof. Alfredo/FINAL 2/data/Budapest_Limpios.csv")
+    dfb = pd.read_csv("data/Barcelona_Limpios.csv")
+    dfc = pd.read_csv("data/Cambridge_Limpios.csv")
+    dfbo = pd.read_csv("data/Boston_Limpios.csv")
+    dfh = pd.read_csv("Hawai_Limpios.csv")
+    dfbu = pd.read_csv("Budapest_Limpios.csv")
     return dfb, dfc, dfbo, dfh, dfbu
