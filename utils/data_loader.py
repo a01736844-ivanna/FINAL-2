0e0f24748e9fc1b5292d0487078528d1683708c3
a01736844-ivanna <a01736844@tec.mx>
2025-11-25 22:00:06 -0600

    data2

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -6,6 +6,6 @@ def load_all_data():
     dfb = pd.read_csv("data/Barcelona_Limpios.csv")
     dfc = pd.read_csv("data/Cambridge_Limpios.csv")
     dfbo = pd.read_csv("data/Boston_Limpios.csv")
-    dfh = pd.read_csv("Hawai_Limpios.csv")
-    dfbu = pd.read_csv("Budapest_Limpios.csv")
+    dfh = pd.read_csv("data/Hawai_Limpios.csv")
+    dfbu = pd.read_csv("data/Budapest_Limpios.csv")
     return dfb, dfc, dfbo, dfh, dfbu
